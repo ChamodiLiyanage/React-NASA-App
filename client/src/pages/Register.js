c6f4294b153a5af5ff2e1c9d2db66c7a1beb72e2
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-05-15 18:59:33 +0530

    Deployment changes

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -12,10 +12,7 @@ function Register() {
   const onFinish = async (values) => {
     try {
       dispatch(showLoading());
-      const response = await axios.post(
-        "https://react-nasa-app.onrender.com/api/user/register",
-        values
-      );
+      const response = await axios.post("/api/user/register", values);
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
